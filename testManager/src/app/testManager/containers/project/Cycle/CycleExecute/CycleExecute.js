3d62c1f1cdddba6bbcde22079801d59c8487ec72
laincarl <1047972853@qq.com>
2018-07-24 15:06:39 +0800

    修复缺陷省略号

--- testManager/src/app/testManager/containers/project/Cycle/CycleExecute/CycleExecute.js
+++ testManager/src/app/testManager/containers/project/Cycle/CycleExecute/CycleExecute.js
@@ -662,6 +662,10 @@ class CycleExecute extends Component {
           </div>
         </Tooltip>);
       },
+<<<<<<< HEAD
+=======
+
+>>>>>>> 修复缺陷省略号
     },
     {
       title: <FormattedMessage id="bug" />,
