5332fa94903423504656042b98ffeccc913781b7
laincarl <1047972853@qq.com>
2018-08-10 11:51:07 +0800

    [IMP]防抖改为lodash

--- testManager/src/app/testManager/containers/project/Cycle/CycleHome/CycleHome.js
+++ testManager/src/app/testManager/containers/project/Cycle/CycleHome/CycleHome.js
@@ -34,25 +34,6 @@ const styles = {
 const dataList = [];
 
 const TreeNode = Tree.TreeNode;
-function debounce(fn, delay) {
-  // 定时器，用来 setTimeout
-  let timer;
-  // 返回一个函数，这个函数会在一个时间区间结束后的 delay 毫秒时执行 fn 函数
-  return function (e) {
-    // 保存函数调用时的上下文和参数，传递给 fn
-    const context = this;
-    // console.log(args);
-    // 每次这个返回的函数被调用，就清除定时器，以保证不执行 fn
-    clearTimeout(timer);
-    // e.persist();
-    const value = e.target.value;
-    // 当返回的函数被最后一次调用后（也就是用户停止了某个连续的操作），
-    // 再过 delay 毫秒就执行 fn
-    timer = setTimeout(() => {
-      fn.call(context, value);
-    }, delay);
-  };
-}
 @observer
 class CycleHome extends Component {
   state = {
@@ -850,7 +831,7 @@ class CycleHome extends Component {
               <div className={leftVisible ? 'c7n-ch-left' : 'c7n-ch-hidden'}>
                 <div className="c7n-chl-head">
                   <div className="c7n-chlh-search">
-                    <Input prefix={prefix} placeholder="过滤" onChange={debounce(this.filterCycle, 200)} />
+                    <Input prefix={prefix} placeholder="过滤" onChange={e => _.debounce(this.filterCycle, 200).call(null, e.target.value)} />
                   </div>
                   <div className="c7n-chlh-button">
                     <div
