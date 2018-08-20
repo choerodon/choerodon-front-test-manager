7b00753079a6121496c6421ceac4ed317a84c758
laincarl <1047972853@qq.com>
2018-08-20 09:49:39 +0800

    [ADD]添加issue Api测试

--- testManager/test/test/apiFunction/issueService/issueFunction.js
+++ testManager/test/test/apiFunction/issueService/issueFunction.js
@@ -21,7 +21,7 @@ const issueFunction = {
         res.should.have.status(200);
         return res;
       })
-  },   
+  },
   /**
    *查询所有的模块
    *
@@ -78,7 +78,7 @@ const issueFunction = {
         return res;
       })
   },  
-    /**
+  /**
    *获取所有issue状态
    *
    * @returns
@@ -91,7 +91,199 @@ const issueFunction = {
         res.should.have.status(200);
         return res;
       })
-  },  
+  },
+  /**
+   * 查询项目的版本
+   */
+  // getVersions(arr) {
+  //   return chai.request(utils.config.gateway)
+  //     .post(`/agile/v1/projects/${projectId}/product_version/names`)     
+  //     .set('Authorization', global.user.token)
+  //     .then((res) => {
+  //       res.should.have.status(200);
+  //       return res;
+  //     })
+  // },
+  /**
+   * 查询项目的史诗
+   */
+  getEpics() {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/issues/epics/select_data`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+  /**
+   * 查询项目的所有冲刺
+   */
+  // getSprints(arr) {
+  //   return chai.request(utils.config.gateway)
+  //     .post(`/agile/v1/projects/${projectId}/sprint/names`)     
+  //     .set('Authorization', global.user.token)
+  //     .then((res) => {
+  //       res.should.have.status(200);
+  //       return res;
+  //     })
+  // },
+  /**
+   * 查询单个项目的冲刺
+   * @param {*} sprintId 
+   */
+  getSprint(sprintId) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/sprint/${sprintId}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+  /**
+   * 查询单个冲刺的issuees
+   * @param {*} sprintId 
+   * @param {*} status 
+   * @param {*} page 
+   * @param {*} size 
+   */
+  getSprintIssues(sprintId, status, page, size) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/sprint/${sprintId}/issues?status=${status}&page=${page}&size=${size}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+  // getChartData(id, type) {
+  //   return chai.request(utils.config.gateway)
+  //     .get(`/agile/v1/projects/${projectId}/reports/${id}/burn_down_report?type=${type}`)     
+  //     .set('Authorization', global.user.token)
+  //     .then((res) => {
+  //       res.should.have.status(200);
+  //       return res;
+  //     })
+  // },
+
+  /**
+   * 查询单个issue
+   * @param {*} issueId 
+   */
+  getIssue(issueId) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/issues/${issueId}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+  /**
+   * 查询子任务
+   * @param {*} issueId 
+   */
+  getSubtask(issueId) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/issues/sub_issue/${issueId}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+  /**
+   * 查询工作日志
+   * @param {*} issueId 
+   */
+  getWorklogs(issueId) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/work_log/issue/${issueId}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+  /**
+   * 查询活动日志
+   * @param {*} issueId 
+   */
+  getDatalogs(issueId) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/data_log?issueId=${issueId}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+  /**
+   * 查询分支
+   * @param {*} issueId 
+   */
+  getBranchs(issueId) {
+    return chai.request(utils.config.gateway)
+      .get(`/devops/v1/project/${projectId}/issue/${issueId}/commit_and_merge_request/count`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+
+  // getIssues(page, size, search, orderField, orderType) {
+  //   return chai.request(utils.config.gateway)
+  //     .get(`/agile/v1/projects/${projectId}/issues/test_component/no_sub_detail?page=${page}&size=${size}`)     
+  //     .set('Authorization', global.user.token)
+  //     .then((res) => {
+  //       res.should.have.status(200);
+  //       return res;
+  //     })
+  // },
+
+
+  /**
+   * 创建链接时查询issues
+   * @param {*} page 
+   * @param {*} size 
+   * @param {*} issueId 
+   * @param {*} content 
+   */
+  getIssuesInLink(page, size, issueId, content) {
+    console.log('en '+ page);
+    let url = content ? `/agile/v1/projects/${projectId}/issues/summary?issueId=${issueId}&self=false&content=${content}&page=${page}&size=${size}&onlyActiveSprint=false`
+      : `/agile/v1/projects/${projectId}/issues/summary?issueId=${issueId}&self=false&page=${page}&size=${size}&onlyActiveSprint=false`;
+
+      return chai.request(utils.config.gateway)
+      .get(url)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
+
+  /**
+   * 查询关联的issues
+   * @param {*} issueId 
+   */
+  getLinkIssues(issueId) {
+    return chai.request(utils.config.gateway)
+      .get(`/agile/v1/projects/${projectId}/issue_links/${issueId}`)     
+      .set('Authorization', global.user.token)
+      .then((res) => {
+        res.should.have.status(200);
+        return res;
+      })
+  },
    
 };
 module.exports = issueFunction;
--- testManager/test/test/apiTest/issueService/issueApi.js
+++ testManager/test/test/apiTest/issueService/issueApi.js
@@ -3,7 +3,8 @@
 const issueFunction = require("../../apiFunction/issueService/issueFunction");
 const utils = require('../../../Utils');
 const projectId = utils.config.projectId;
-const { getIssuesSearch, getProjectVersion, getModules, getLabels, getPrioritys, getIssueStatus } = issueFunction;
+// const { getIssuesSearch, getProjectVersion, getModules, getLabels, getPrioritys, getIssueStatus,  } = issueFunction;
+const issueId = 9058;
 describe("Issue Api-GET", () => {
   it("[GET] 根据筛选条件查询issue", () => {
     const search = {
@@ -16,21 +17,79 @@ describe("Issue Api-GET", () => {
         // version: [version.versionId],
       },
     }
-    return getIssuesSearch(search);
+    return issueFunction.getIssuesSearch(search);
   });
   it("[GET] 查询项目下所有版本", () => {
-    return getProjectVersion();
+    return issueFunction.getProjectVersion();
   });
   it("[GET] 查询项目下所有模块", () => {
-    return getModules();
+    return issueFunction.getModules();
   });
   it("[GET] 查询项目下所有标签", () => {
-    return getLabels();
+    return issueFunction.getLabels();
   });
   it("[GET] 查询项目下所有优先级", () => {
-    return getPrioritys();
+    return issueFunction.getPrioritys();
   });
   it("[GET] 查询项目下所有issue状态", () => {
-    return getIssueStatus();
+    return issueFunction.getIssueStatus();
   });
+
+  // it("[GET] 查询项目的版本", () => {
+  //   return issueFunction.getVersions(arr = []);
+  // });
+
+  it("[GET] 查询项目的史诗", () => {
+    return issueFunction.getEpics();
+  });
+
+  // it("[GET] 查询项目的所有冲刺", () => {
+  //   return issueFunction.getSprints(arr = []);
+  // });
+
+  // it("[GET] 查询单个项目的冲刺", () => {
+  //   return issueFunction.getSprint(sprintId);
+  // });
+
+  // it("[GET] 查询单个冲刺的issuees", () => {
+  //   return issueFunction.getSprintIssues(sprintId, status, page = 0, size = 99999);
+  // });
+
+  // it("[GET] 查询", () => {
+  //   return issueFunction.getChartData(id, type);
+  // });
+
+  it("[GET] 查询单个issue", () => {
+    return issueFunction.getIssue(issueId);
+  });
+
+  it("[GET] 查询子任务", () => {
+    return issueFunction.getSubtask(issueId);
+  });
+
+  it("[GET] 查询工作日志", () => {
+    return issueFunction.getWorklogs(issueId);
+  });
+
+  it("[GET] 查询活动日志", () => {
+    return issueFunction.getDatalogs(issueId);
+  });
+
+  it("[GET] 查询分支", () => {
+    return issueFunction.getBranchs(issueId);
+  });
+
+  // it("[GET] 查询项目的图片", () => {
+  //   return issueFunction.getIssues(page = 0, size = 10, search, orderField, orderType);
+  // });
+  
+  it("[GET] 创建链接时查询issues", () => {
+    let page = 0, size = 10, content;
+    return issueFunction.getIssuesInLink(page, size, issueId, content);
+  });
+  
+  it("[GET] 查询关联的issues", () => {
+    return issueFunction.getLinkIssues(issueId);
+  });
+  
 });
