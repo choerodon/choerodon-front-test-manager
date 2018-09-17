cbd30db354b95eadc416b52c6a3e58f94279e4a2
laincarl <1047972853@qq.com>
2018-09-17 17:39:22 +0800

    [FIX]报表跳转到执行详情改为不可编辑

--- testManager/src/app/testManager/containers/project/Report/ReportStory/ReportStory.js
+++ testManager/src/app/testManager/containers/project/Report/ReportStory/ReportStory.js
@@ -472,7 +472,7 @@ class ReportStory extends Component {
                     </div>
                     <Link
                       style={{ lineHeight: '13px' }}
-                      to={`/testManager/TestExecute/execute/${execute.executeId}?type=${urlParams.type}&id=${urlParams.id}&name=${urlParams.name}`}
+                      to={`/testManager/TestExecute/executeShow/${execute.executeId}?type=${urlParams.type}&id=${urlParams.id}&name=${urlParams.name}`}
                     >
                       <Icon type="explicit2" style={{ marginLeft: 10, color: 'black' }} />
                     </Link>
