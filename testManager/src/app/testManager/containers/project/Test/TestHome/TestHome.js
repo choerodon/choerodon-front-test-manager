a9f6cbce05fd2e17ee1a3883772cbfb8d4d25c79
laincarl <1047972853@qq.com>
2018-09-29 11:05:12 +0800

    [FIX]进行中改为处理中

--- testManager/src/app/testManager/containers/project/Test/TestHome/TestHome.js
+++ testManager/src/app/testManager/containers/project/Test/TestHome/TestHome.js
@@ -328,7 +328,7 @@ class Test extends Component {
             value: 'todo',
           },
           {
-            text: Choerodon.getMessage('进行中', 'doing'),
+            text: Choerodon.getMessage('处理中', 'doing'),
             value: 'doing',
           },
           {
