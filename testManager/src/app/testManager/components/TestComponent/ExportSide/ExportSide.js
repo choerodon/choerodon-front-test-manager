49f92725a5ca92c890a5f22f4475cdd3bf16f2cc
laincarl <1047972853@qq.com>
2018-10-29 10:09:24 +0800

    [FIX]布局以及展示信息更改

--- testManager/src/app/testManager/components/TestComponent/ExportSide/ExportSide.js
+++ testManager/src/app/testManager/components/TestComponent/ExportSide/ExportSide.js
@@ -5,6 +5,7 @@ import {
   Modal, Progress, Table, Select, Button, Icon,
 } from 'choerodon-ui';
 import FileSaver from 'file-saver';
+import moment from 'moment';
 import { SelectVersion, SelectFolder } from '../../CommonComponent';
 import { exportIssues } from '../../../api/IssueManageApi';
 import './ExportSide.scss';
@@ -12,6 +13,27 @@ import './ExportSide.scss';
 const Option = Select.Option;
 const { Sidebar } = Modal;
 const { AppState } = stores;
+function humanizeDuration(timeInMillisecond) {
+  let result = '';
+  if (timeInMillisecond) {
+    if ((result = Math.round(timeInMillisecond / (60 * 60 * 24 * 30 * 12))) > 0) { // year
+      result = `${result} 年`;
+    } else if ((result = Math.round(timeInMillisecond / (60 * 60 * 24 * 30))) > 0) { // months
+      result = `${result} 月`;
+    } else if ((result = Math.round(timeInMillisecond / (60 * 60 * 24))) > 0) { // days
+      result = `${result} 天`;
+    } else if ((result = Math.round(timeInMillisecond / (60 * 60))) > 0) { // Hours
+      result = `${result} 小时`;
+    } else if ((result = Math.round(timeInMillisecond / (60))) > 0) { // minute
+      result = `${result} 分钟`;
+    } else if ((result = Math.round(timeInMillisecond)) > 0) { // second
+      result = `${result} 秒`;
+    } else {
+      result = `${timeInMillisecond} 毫秒`;
+    }
+  }
+  return result;
+}
 class ExportSide extends Component {
   state = {
     visible: false,
@@ -68,14 +90,14 @@ class ExportSide extends Component {
     });
   }
 
-  handleVersionChange=(versionId) => {
+  handleVersionChange = (versionId) => {
     this.setState({
       versionId,
       folderId: null,
     });
   }
 
-  handleFolderChange=(folderId) => {
+  handleFolderChange = (folderId) => {
     this.setState({
       folderId,
     });
@@ -84,13 +106,37 @@ class ExportSide extends Component {
   render() {
     const { visible, versionId, folderId } = this.state;
     const data = [{
-      source: '测试管理开发项目',
+      source: '项目：测试管理开发项目',
+      version: '0.1.0',
+      folder: '文件夹',
+      num: 10,
+      time: '2018-10-25',
+      during: 30,
+      progress: 50,
+      file: {
+        name: '测试管理开发项目.xlsx',
+        url: 'http://minio.staging.saas.hand-china.com/error-member-role/file_414d93294456483aa81fbfefef92d79e_errorMemberRole.xls',
+      },
+    }, {
+      source: '版本：0.1.0',
       version: '0.1.0',
       folder: '文件夹',
       num: 10,
       time: '2018-10-25',
       during: 100,
-      progress: 50,
+      progress: 100,
+      file: {
+        name: '测试管理开发项目.xlsx',
+        url: 'http://minio.staging.saas.hand-china.com/error-member-role/file_414d93294456483aa81fbfefef92d79e_errorMemberRole.xls',
+      },
+    }, {
+      source: '文件夹：新文件夹',
+      version: '0.1.0',
+      folder: '文件夹',
+      num: 10,
+      time: '2018-10-25',
+      during: 1000,
+      progress: 100,
       file: {
         name: '测试管理开发项目.xlsx',
         url: 'http://minio.staging.saas.hand-china.com/error-member-role/file_414d93294456483aa81fbfefef92d79e_errorMemberRole.xls',
@@ -102,7 +148,7 @@ class ExportSide extends Component {
       key: 'source',
       width: 100,
       render: source => <div className="c7ntest-text-dot">{source}</div>,
-    }, 
+    },
     // {
     //   title: '版本',
     //   dataIndex: 'version',
@@ -121,26 +167,35 @@ class ExportSide extends Component {
       title: '导出时间',
       dataIndex: 'time',
       key: 'time',
-      width: 150,
+      width: 160,
+      render: () => moment().format('YYYY-MM-DD h:mm:ss'),
     }, {
       title: '耗时',
       dataIndex: 'during',
       key: 'during',
       width: 100,
+      render: during => <div>{humanizeDuration(during)}</div>,
     }, {
       title: '进度',
       dataIndex: 'progress',
       key: 'progress',
-      render: () => <Progress percent={50} status="active" size="small" showInfo={false} />,
+      render: progress => (progress === 100
+        ? <div>已完成</div>
+        : (
+          <div>
+            {progress}
+            %
+          </div>
+        )),
     }, {
       title: '下载',
       dataIndex: 'file',
       key: 'file',
-      render: file => (
+      render: (file, record) => (
         // <a className="c7ntext-text-dot" href={file.url}>
-        
-        <Button shape="circle" funcType="flat" icon="get_app" />
-        
+
+        <Button shape="circle" funcType="flat" icon="get_app" disabled={record.progress < 100} />
+
         // </a>
       ),
     }];
@@ -161,14 +216,14 @@ class ExportSide extends Component {
           description={<FormattedMessage id="export_side_content_description" />}
           link="http://v0-8.choerodon.io/zh/docs/user-guide/test-management"
         >
-          <div className="c7ntest-ExportSide"> 
-            <SelectVersion allowClear value={versionId} onChange={this.handleVersionChange} />            
+          <div className="c7ntest-ExportSide">
+            <SelectVersion allowClear value={versionId} onChange={this.handleVersionChange} />
             <SelectFolder label="文件夹" versionId={versionId} value={folderId} allowClear onChange={this.handleFolderChange} />
-             
-           
-            <Button type="primary" funcType="raised">新建导出</Button>       
+
+
+            <Button type="primary" funcType="raised">新建导出</Button>
             {/* {this.renderRecord()} */}
-            <Table columns={columns} dataSource={data} />          
+            <Table columns={columns} dataSource={data} />
           </div>
         </Content>
       </Sidebar>
