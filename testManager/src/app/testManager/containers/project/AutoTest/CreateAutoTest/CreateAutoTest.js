f84257fa0bf1c5395a10bc6c106ff3588c1c2e27
laincarl <1047972853@qq.com>
2018-11-20 11:58:05 +0800

    [FIX]优化选择应用

--- testManager/src/app/testManager/containers/project/AutoTest/CreateAutoTest/CreateAutoTest.js
+++ testManager/src/app/testManager/containers/project/AutoTest/CreateAutoTest/CreateAutoTest.js
@@ -4,10 +4,9 @@ import { injectIntl, FormattedMessage } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { Steps } from 'choerodon-ui';
 import {
-  Content, Header, Page, stores, axios, 
+  Content, Header, Page, stores, 
 } from 'choerodon-front-boot';
 import _ from 'lodash';
-
 import './CreateAutoTest.scss';
 import { commonLink } from '../../../../common/utils';
 import CreateAutoTestStore from '../../../../store/project/AutoTest/CreateAutoTestStore';
@@ -18,16 +17,6 @@ const { AppState } = stores;
 
 @observer
 class CreateAutoTest extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {     
-      mode: 'new',    
-      markers: null,
-      loading: false,
-      changeYaml: false,
-    };
-  }
-
   componentDidMount() {
     // const { CreateAutoTestStore } = this.props;
     // CreateAutoTestStore.setValue(null);
@@ -60,134 +49,62 @@ class CreateAutoTest extends Component {
     // CreateAutoTestStore.loadEnv();
   }
 
-
   /**
-   * 获取步骤条状态
-   * @param index
-   * @returns {string}
-   */
-  getStatus = (index) => {
-    const currentStep = CreateAutoTestStore.currentStep;
-    let status = 'process';
-    if (index === currentStep) {
-      status = 'process';
-    } else if (index > currentStep) {
-      status = 'wait';
-    } else {
-      status = 'finish';
-    }
-    return status;
-  };
-
-  /**
-   * 改变步骤条
-   * @param index
-   */
-  changeStep = (index) => {
-    const { CreateAutoTestStore } = this.props;
-    const {
-      appId, appVersionId, envId, mode, 
-    } = this.state;
-    this.setState({ currentStep: index });
-    this.loadReview();
-    // 加载yaml
-    if (index === 2 && appId) {
-      CreateAutoTestStore.setValue(null);
-      CreateAutoTestStore.loadValue(appId, appVersionId, envId)
-        .then((data) => {
-          this.setState({ errorLine: data.errorLines });
-        });
-    }
-  };
-
-
-  /**
-   * 修改实例模式
-   * @param value
-   */
-  handleChangeMode = (value) => {
-    this.setState({ mode: value.target.value });
-  };
-
-  /**
-   *选择目标版本
-   *
-   * @param {*} versionId
-   * @memberof CreateAutoTest
-   */
-  // handleVersionSelect=(versionId) => {
+     * 取消第一步
+     */
+  // clearStepOne = () => {
+  //   CreateAutoTestStore.setVersions([]);
+  //   CreateAutoTestStore.setValue(null);
   //   this.setState({
-  //     versionId,
+  //     currentStep: 1,
+  //     appId: undefined,
+  //     app: null,
+  //     appVersionId: undefined,
+  //     versionDto: null,
+  //     envId: undefined,
+  //     envDto: null,
+  //     value: null,
+  //     yaml: null,
+  //     markers: [],
+  //     mode: 'new',
+  //     instanceId: undefined,
+  //     changeYaml: false,
   //   });
-  // }
+  // };
 
   /**
-   * 取消第一步
-   */
-  clearStepOne = () => {
-    const { CreateAutoTestStore } = this.props;
-    CreateAutoTestStore.setVersions([]);
-    CreateAutoTestStore.setValue(null);
-    this.setState({
-      currentStep: 1,
-      appId: undefined,
-      app: null,
-      appVersionId: undefined,
-      versionDto: null,
-      envId: undefined,
-      envDto: null,
-      value: null,
-      yaml: null,
-      markers: [],
-      mode: 'new',
-      instanceId: undefined,
-      changeYaml: false,
-    });
-  };
-
-  /**
-   * 取消第一步
-   */
-  clearStepOneBack = () => {
-    const { location } = this.props;
-    CreateAutoTestStore.setVersions([]);
-    CreateAutoTestStore.setValue(null);
-    this.setState({
-      currentStep: 1,
-      appId: undefined,
-      app: null,
-      appVersionId: undefined,
-      versionDto: null,
-      envId: undefined,
-      envDto: null,
-      value: null,
-      yaml: null,
-      markers: [],
-      mode: 'new',
-      instanceId: undefined,
-      changeYaml: false,
-    });
-    if (location.search.indexOf('envId') !== -1) {
-      const { history } = this.props;
-      history.go(-1);
-    }
-  };
-
-  loadReview = async () => {
-    const { value, appVersionId, projectId } = this.state;
-    if (value) {
-      const yaml = await axios.post(`/devops/v1/projects/${projectId}/app_instances/previewValue?appappVersionId=${appVersionId}`, { yaml: value })
-        .then(data => data);
-      this.setState({ yaml });
-    }
-  };
-
+     * 取消第一步
+     */
+  // clearStepOneBack = () => {
+  //   const { location } = this.props;
+  //   CreateAutoTestStore.setVersions([]);
+  //   CreateAutoTestStore.setValue(null);
+  //   this.setState({
+  //     currentStep: 1,
+  //     appId: undefined,
+  //     app: null,
+  //     appVersionId: undefined,
+  //     versionDto: null,
+  //     envId: undefined,
+  //     envDto: null,
+  //     value: null,
+  //     yaml: null,
+  //     markers: [],
+  //     mode: 'new',
+  //     instanceId: undefined,
+  //     changeYaml: false,
+  //   });
+  //   if (location.search.indexOf('envId') !== -1) {
+  //     const { history } = this.props;
+  //     history.go(-1);
+  //   }
+  // };
   render() {
     const { intl } = this.props;
     const { formatMessage } = intl;
     const data = CreateAutoTestStore.value;
     const projectName = AppState.currentMenuType.name;
-    const { currentStep, appVersionId, env } = CreateAutoTestStore;
+    const { currentStep } = CreateAutoTestStore;
     const toStep = CreateAutoTestStore.toStep;
     return (
       <Page
@@ -195,33 +112,27 @@ class CreateAutoTest extends Component {
       >
         <Header
           title={<FormattedMessage id="autotest_create_header_title" />}
-          backPath={commonLink('/AutoTest/list')} 
+          backPath={commonLink('/AutoTest/list')}
         />
         <Content className="c7ntest-deployApp-wrapper" code="autotest" values={{ name: projectName }}>
           <div className="deployApp-card">
-            <Steps currentStep={this.state.currentStep}>
+            <Steps current={currentStep - 1}>
               <Step
                 title={<span style={{ color: currentStep === 1 ? '#3F51B5' : '', fontSize: 14 }}>{formatMessage({ id: 'autoteststep_one_title' })}</span>}
-                onClick={() => { toStep(1); }}
-                status={this.getStatus(1)}
               />
               <Step
-                className={!(appVersionId) ? 'step-disabled' : ''}
+                // className={!(appVersionId) ? 'step-disabled' : ''}
                 title={<span style={{ color: currentStep === 2 ? '#3F51B5' : '', fontSize: 14 }}>{formatMessage({ id: 'autoteststep_two_title' })}</span>}
-                onClick={() => { toStep(2); }}
-                status={this.getStatus(2)}
               />
               <Step
                 // className={!((mode === 'new' || (mode === 'replace' && instanceId)) && this.state.envId) ? 'step-disabled' : ''}
                 title={<span style={{ color: currentStep === 3 ? '#3F51B5' : '', fontSize: 14 }}>{formatMessage({ id: 'autoteststep_three_title' })}</span>}
-                onClick={() => { toStep(3); }}
-                status={this.getStatus(3)}
               />
             </Steps>
-            <div className="deployApp-card-content"> 
+            <div className="deployApp-card-content">
               {currentStep === 1 && <SelectVariable />}
               {currentStep === 2 && <ModifyConfig />}
-              {currentStep === 3 && <ConfirmInfo />}              
+              {currentStep === 3 && <ConfirmInfo />}
             </div>
           </div>
 
