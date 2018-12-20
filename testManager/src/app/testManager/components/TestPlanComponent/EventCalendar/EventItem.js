0f4bc9ef794189359c6cf7b854fe0e7721130921
laincarl <1047972853@qq.com>
2018-12-20 09:05:03 +0800

    [FIX]修复

--- testManager/src/app/testManager/components/TestPlanComponent/EventCalendar/EventItem.js
+++ testManager/src/app/testManager/components/TestPlanComponent/EventCalendar/EventItem.js
@@ -121,7 +121,7 @@ class EventItem extends Component {
         }}
       >
         {canReasize.includes(type) && <div className="c7ntest-EventItem-event-resizer-left" style={{ left: preFlex === 0 ? 0 : -10 }} onMouseDown={this.handleMouseDown.bind(this, 'left')} ref={this.saveRef('left')} role="none" />}
-        {canReasize.includes(type) && <div className="c7ntest-EventItem-event-resizer-right" style={{ left: lastFlex === 0 ? 0 : -10 }} onMouseDown={this.handleMouseDown.bind(this, 'right')} ref={this.saveRef('right')} role="none" />}
+        {canReasize.includes(type) && <div className="c7ntest-EventItem-event-resizer-right" style={{ right: lastFlex === 0 ? 0 : -10 }} onMouseDown={this.handleMouseDown.bind(this, 'right')} ref={this.saveRef('right')} role="none" />}
         <Tooltip getPopupContainer={() => findDOMNode(this)} title={tipTitle} placement="topLeft">
           <div className="c7ntest-EventItem-event-title c7ntest-text-dot">
             {title}            
