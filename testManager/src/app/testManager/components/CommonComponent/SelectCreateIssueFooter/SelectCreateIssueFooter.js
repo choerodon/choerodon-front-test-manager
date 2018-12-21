492c56d117e8981ab97c4b574fc89aa164afe8b9
laincarl <1047972853@qq.com>
2018-12-21 14:21:36 +0800

    [FIX]组件位置移动

--- testManager/src/app/testManager/components/CommonComponent/SelectCreateIssueFooter/SelectCreateIssueFooter.js
+++ testManager/src/app/testManager/components/CommonComponent/SelectCreateIssueFooter/SelectCreateIssueFooter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-import { createIssueLink } from '../../common/utils';
+import { createIssueLink } from '../../../common/utils';
 
 const SelectCreateIssueFooter = () => (
   <Link style={{ color: ' #3F51B5' }} to={createIssueLink()} target="_blank">
