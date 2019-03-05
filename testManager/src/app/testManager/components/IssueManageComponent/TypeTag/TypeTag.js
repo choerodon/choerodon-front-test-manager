c3cb62acd35b9ac8dcf117317552d1129028397f
laincarl <1047972853@qq.com>
2019-03-05 15:53:40 +0800

    [FIX]修改typetag

--- testManager/src/app/testManager/components/IssueManageComponent/TypeTag/TypeTag.js
+++ testManager/src/app/testManager/components/IssueManageComponent/TypeTag/TypeTag.js
@@ -1,40 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Icon } from 'choerodon-ui';
+import './TypeTag.scss';
 
-class TypeTag extends Component {
-  render() {
-    const { type } = this.props;
-    const {
-      colour: typeColor, name: typeName, typeCode, icon,
-    } = type || {}; 
-    const backgroundColor = typeColor && typeColor.includes('#') ? typeColor : `#${typeColor}`;
-    return (
-      <div style={{ display: 'flex' }}>
-        <div      
-          style={{
-            backgroundColor,
-            display: 'flex',
-            width: 20,
-            height: 20,
-            borderRadius: '4px',
-            color: '#fff',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+const initTypes = ['agile_epic', 'agile_story', 'agile_fault', 'agile_task', 'agile_subtask'];
+const TypeTag = ({
+  type, style, showName,
+}) => {
+  const {
+    colour: typeColor, name: typeName, typeCode, icon,
+  } = type || {}; 
+  const backgroundColor = typeColor && typeColor.includes('#') ? typeColor : `#${typeColor}`;
+  return (
+    <div className="c7n-typeTag" style={style}>
+      {initTypes.indexOf(icon || '') !== -1
+        ? (
           <Icon
-            style={{ fontSize: '14px' }}
-            type={icon}
-            color="#fff"
+            style={{
+              fontSize: '26px',
+              color: typeColor || '#fab614',
+            }}
+            type={icon || 'help'}
           />
-        </div>
-        {
-          this.props.showName && (
-            <span style={{ marginLeft: 8 }}>{typeName}</span>
-          )
-        }
-      </div>
-    );
-  }
-}
+        )
+        : (
+          <div
+            className="icon-wapper"
+            style={{
+              backgroundColor: typeColor || '#fab614',
+              padding: '2px',
+              margin: '3px',
+            }}
+          >
+            <Icon
+              style={{ fontSize: '16px' }}
+              type={icon || 'help'}
+            />
+          </div>
+        )
+      }
+      {
+        showName && (
+          <span className="name">{typeName || ''}</span>
+        )
+      }
+    </div>
+  );
+};
 export default TypeTag;
