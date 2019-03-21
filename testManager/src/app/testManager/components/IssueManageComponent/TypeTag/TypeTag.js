8783028e7f389cd5866a770c95ad6ba1f217ffc8
laincarl <1047972853@qq.com>
2019-03-21 16:50:22 +0800

    [FIX]修复执行详情图标问题

--- testManager/src/app/testManager/components/IssueManageComponent/TypeTag/TypeTag.js
+++ testManager/src/app/testManager/components/IssueManageComponent/TypeTag/TypeTag.js
@@ -3,42 +3,21 @@ import isEqual from 'react-fast-compare';
 import { Icon } from 'choerodon-ui';
 import './TypeTag.scss';
 
-const initTypes = ['agile_epic', 'agile_story', 'agile_fault', 'agile_task', 'agile_subtask'];
 const TypeTag = ({
   type, style, showName,
 }) => {
   const {
     colour: typeColor, name: typeName, typeCode, icon,
   } = type || {}; 
-  const backgroundColor = typeColor && typeColor.includes('#') ? typeColor : `#${typeColor}`;
   return (
     <div className="c7ntest-typeTag" style={style}>
-      {initTypes.indexOf(icon || '') !== -1
-        ? (
-          <Icon
-            style={{
-              fontSize: '26px',
-              color: typeColor || '#fab614',
-            }}
-            type={icon || 'help'}
-          />
-        )
-        : (
-          <div
-            className="icon-wapper"
-            style={{
-              backgroundColor: typeColor || '#fab614',
-              padding: '2px',
-              margin: '3px',
-            }}
-          >
-            <Icon
-              style={{ fontSize: '16px' }}
-              type={icon || 'help'}
-            />
-          </div>
-        )
-      }
+      <Icon
+        style={{
+          fontSize: '26px',
+          color: typeColor || '#fab614',
+        }}
+        type={icon || 'help'}
+      />     
       {
         showName && (
           <span className="name">{typeName || ''}</span>
